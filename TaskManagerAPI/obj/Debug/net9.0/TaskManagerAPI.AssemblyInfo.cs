2b6c95e86db08152330b361a94723c7314ac23d1
Thandeka Lubisi <thandekachrtabel@gmail.com>
2025-08-17 23:44:30 +0200

    Initial commit - Task Manager App

--- TaskManagerAPI/obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
+++ TaskManagerAPI/obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e598b02e03b4415b529602959ace449ab467bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
